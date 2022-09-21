d6e9a0d6949dcce00e06c74b88c08552a2ad4375
Gabriel-Rauch <113104512+Gabriel-Rauch@users.noreply.github.com>
2022-09-21 19:49:40 +0000

    testes inicial de compartilhamento

---
+++ index.js
@@ -0,0 +1,55 @@
+//variaveis
+nomes = ['Rhoger', 'Cléverson', 'Gleysberto', 'Nádio', 'Rosiméri', 'Regislane', 'Agripino', 'Wellison']
+idade = [69,66,58,61,60,64,59,55]
+trabalho = [31,29,30,26,25,24,26,29]
+
+//funções
+function aposentadoria(nome)
+{
+  //variaveis da função
+   nomenalista = false
+   anos=0
+   tempotrabalho = 0
+
+ for (ordem = 0; ordem < nomes.length; ordem++)
+ {
+   
+   //execusão
+   if(nome==nomes[ordem])
+   {
+     nomenalista = true
+     anos = idade[ordem]
+     tempotrabalho = trabalho[ordem]
+   }
+ }
+
+
+ //execusão
+ if (nomenalista)
+ {
+   if (anos >= 65 || tempotrabalho >=30 ||anos >=60 && tempotrabalho >=25)
+    {
+      console.log('parabens tem direito a aposentadoria')
+    }
+   else
+    {
+      console.log('infelizmente voce nao tem direito a aposentadoria')
+    }
+ }
+ else 
+ {
+  console.log('nome invalido') 
+ }
+}
+ aposentadoria('Rosiméri')    
+ aposentadoria('Agripino')    
+ aposentadoria('Rhoger')      
+ aposentadoria('Hudnélson')   
+ aposentadoria('Gleysberto')  
+ aposentadoria('Regislane')   
+ aposentadoria('Cléverson')   
+ aposentadoria('Nádio')       
+ aposentadoria('Wellison')    
+ aposentadoria('Florisberto') 
+
+
